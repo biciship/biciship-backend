23cf044741371515cce813b3772d125e6dd0488d
biciship <biciship@gmail.com>
2025-04-23 16:17:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)

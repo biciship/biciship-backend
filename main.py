b7a237fc35c32efc40d9c7dc5c313b031babc86b
biciship <biciship@gmail.com>
2025-04-23 16:36:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,3 @@ def root():
     return {"message": "Biciship API funcionando 🚲"}
 
 import os
-import uvicorn 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)

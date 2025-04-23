4eec2a21bdfd44b9000378bc967f98812dc96151
biciship <biciship@gmail.com>
2025-04-23 17:19:35 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI
 from app.routers import bikes, users, transport_jobs
 from app.db.database import database
@@ -11,11 +10,15 @@ app.include_router(transport_jobs.router, prefix="/transport-jobs", tags=["Trans
 
 @app.on_event("startup")
 async def startup():
+    print("🔥 Iniciando conexión con la base de datos...")
     await database.connect()
+    print("✅ Conectado a la base de datos.")
 
 @app.on_event("shutdown")
 async def shutdown():
+    print("📴 Cerrando conexión con la base de datos...")
     await database.disconnect()
+    print("✅ Conexión cerrada.")
 
 @app.get("/")
 def root():

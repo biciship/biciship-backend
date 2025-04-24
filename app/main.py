302d2609ceeb932ed6235f94eb864848c0cc4b20
biciship <biciship@gmail.com>
2025-04-24 17:48:23 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from app.routers import users, bikes, transport_jobs
+from app.db.database import database
 
-app = FastAPI()
+app = FastAPI(title="Biciship API")
+
+app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(bikes.router, prefix="/bikes", tags=["Bikes"])
+app.include_router(transport_jobs.router, prefix="/transport-jobs", tags=["Transport Jobs"])
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 @app.get("/")
 def root():
-    return {"message": "¡Funciona desde Cloud Run!"}
+    return {"message": "Biciship API funcionando 🚲"}

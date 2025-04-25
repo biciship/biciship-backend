d8aaac1059aae280da95ba992cc7842aa84a8563
biciship <biciship@gmail.com>
2025-04-25 12:49:35 +0200

    Update users.py

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from app.db.database import database
+from app.db.models import users
+from sqlalchemy import insert, select, delete
 
 router = APIRouter()
 
 @router.get("/")
 async def get_users():
-    return {"users": []}
+    query = select(users)
+    return await database.fetch_all(query)
+
+@router.post("/")
+async def create_user(payload: dict):
+    query = insert(users).values(name=payload["name"], email=payload["email"])
+    last_record_id = await database.execute(query)
+    return {"id": last_record_id}
+
+@router.delete("/{user_id}")
+async def delete_user(user_id: int):
+    query = delete(users).where(users.c.id == user_id)
+    result = await database.execute(query)
+    if result:
+        return {"message": "User deleted"}
+    raise HTTPException(status_code=404, detail="User not found")

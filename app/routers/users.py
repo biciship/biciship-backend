7b8e1a23080767b66f008b1e00cf12af60998b55
biciship <biciship@gmail.com>
2025-05-05 14:16:10 +0200

    Update users.py

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,40 +1,21 @@
 from fastapi import APIRouter, HTTPException, Depends, status
-from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy import insert, select, delete
 from app.db.database import database
 from app.db.models import users
-from app.auth.utils import decode_access_token
-from sqlalchemy import insert, select, delete
-from app.auth.dependencies import require_role  # 👈 añadimos esto
+from app.auth.dependencies import require_role
 
 router = APIRouter()
-
-# Configuración de seguridad OAuth2
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
-
-# Función para verificar el token
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    payload = decode_access_token(token)
-    if payload is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token inválido o expirado",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return payload
-
-# --- Endpoints ---
 
 @router.get("/")
 async def get_users(dep=Depends(require_role(["admin", "operador"]))):
     query = select(users)
     return await database.fetch_all(query)
 
-
 @router.post("/")
 async def create_user(payload: dict):
     name = payload.get("name")
     email = payload.get("email")
-    role = payload.get("role", "cliente")  # default si no lo mandan
+    role = payload.get("role", "cliente")
 
     if not name or not email:
         raise HTTPException(status_code=400, detail="Name and email are required")
@@ -44,9 +25,7 @@ async def create_user(payload: dict):
     return {"id": last_record_id}
 
 @router.delete("/{user_id}")
-async def delete_user(
-    user_id: int,
-    dep=Depends(require_role(["admin", "operador"]))):
+async def delete_user(user_id: int, dep=Depends(require_role(["admin", "operador"]))):
     query = delete(users).where(users.c.id == user_id)
     result = await database.execute(query)
     if result:

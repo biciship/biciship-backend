072f50982e3cc0812fce82b935ddb0c3ddf126d1
biciship <biciship@gmail.com>
2025-05-08 16:32:00 +0200

    Update health.py

--- app/health.py
+++ app/health.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi import APIRouter
+import os
 
 app = FastAPI()
+router = APIRouter()
 
 @app.get("/")
 def read_root():
     return {"message": "Health OK"}
+
+@router.get("/debug")
+async def debug():
+    return {
+        "DATABASE_URL": os.getenv("DATABASE_URL"),
+        "SECRET_KEY": os.getenv("SECRET_KEY")
+    }

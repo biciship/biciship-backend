442f7968800a51b7ca32e2ffdf799b751531a884
biciship <biciship@gmail.com>
2025-05-09 13:25:18 +0200

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -3,7 +3,8 @@ import os
 from databases import Database
 
 #load_dotenv()  # ✅ esta línea debe ir antes de os.getenv()
-print("DB URL desde entorno:", os.getenv("DATABASE_URL"))
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DB URL desde entorno:", os.getenv("DATABASE_URL"))
 database = Database(DATABASE_URL)
